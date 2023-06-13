74d76737222e2fc1ff5f4ad56beeb78b92176ec8
hami <hamyil@pm.me>
2023-06-13 19:55:54 +0200

    -> updated test data with some pictures and restaurant names

--- src/Factory/RestaurantFactory.php
+++ src/Factory/RestaurantFactory.php
@@ -29,9 +29,90 @@ use Zenstruck\Foundry\RepositoryProxy;
  */
 final class RestaurantFactory extends ModelFactory
 {
-    private const RANDOM_PHOT_PATH = [
-        'assets/img/restaurants/logo_hakis.png'
+    private const BACKGROUND_PHOTOS = [
+        'assets/img/restaurants/backgrounds/2_brueder.jpg',
+        'assets/img/restaurants/backgrounds/amore.jpg',
+        'assets/img/restaurants/backgrounds/asia_china_imbiss.jpg',
+        'assets/img/restaurants/backgrounds/asia_thai.jpg',
+        'assets/img/restaurants/backgrounds/athos.jpg',
+        'assets/img/restaurants/backgrounds/background_hakis.png',
+        'assets/img/restaurants/backgrounds/beirut.jpg',
+        'assets/img/restaurants/backgrounds/celery.jpg',
+        'assets/img/restaurants/backgrounds/chicos.jpg',
+        'assets/img/restaurants/backgrounds/fine.jpg',
+        'assets/img/restaurants/backgrounds/fortuna.jpg',
+        'assets/img/restaurants/backgrounds/gold_dragon.jpg',
+        'assets/img/restaurants/backgrounds/hakis.jpg',
+        'assets/img/restaurants/backgrounds/holy rice.jpg',
+        'assets/img/restaurants/backgrounds/indisch.png',
+        'assets/img/restaurants/backgrounds/just_lecker.jpg',
+        'assets/img/restaurants/backgrounds/khaohom_thai.jpg',
+        'assets/img/restaurants/backgrounds/orchid.jpg',
+        'assets/img/restaurants/backgrounds/pak-royal.jpg',
+        'assets/img/restaurants/backgrounds/pizza_cab.jpg',
+        'assets/img/restaurants/backgrounds/round_two.jpg',
+        'assets/img/restaurants/backgrounds/royal_3.jpg',
+        'assets/img/restaurants/backgrounds/sinbads.jpg',
+        'assets/img/restaurants/backgrounds/tandoori_palace.jpg',
+        'assets/img/restaurants/backgrounds/zu_dis_asia.jpg'
     ];
+
+    private const LOGOS = [
+        'assets/img/restaurants/logos/2_brueder.png',
+        'assets/img/restaurants/logos/amore.png',
+        'assets/img/restaurants/logos/asia_china_imbiss.png',
+        'assets/img/restaurants/logos/asia_thai.png',
+        'assets/img/restaurants/logos/athos.png',
+        'assets/img/restaurants/logos/background_hakis.png',
+        'assets/img/restaurants/logos/beirut.png',
+        'assets/img/restaurants/logos/celery.png',
+        'assets/img/restaurants/logos/chicos.png',
+        'assets/img/restaurants/logos/fine.png',
+        'assets/img/restaurants/logos/fortuna.png',
+        'assets/img/restaurants/logos/gold_dragon.png',
+        'assets/img/restaurants/logos/hakis.png',
+        'assets/img/restaurants/logos/holy rice.png',
+        'assets/img/restaurants/logos/indisch.png',
+        'assets/img/restaurants/logos/just_lecker.png',
+        'assets/img/restaurants/logos/khaohom_thai.png',
+        'assets/img/restaurants/logos/orchid.png',
+        'assets/img/restaurants/logos/pak-royal.png',
+        'assets/img/restaurants/logos/pizza_cab.png',
+        'assets/img/restaurants/logos/round_two.png',
+        'assets/img/restaurants/logos/royal_3.png',
+        'assets/img/restaurants/logos/sinbads.png',
+        'assets/img/restaurants/logos/tandoori_palace.png',
+        'assets/img/restaurants/logos/zu_dis_asia.png'
+    ];
+
+    private const NAMES = [
+        '2_brueder',
+        'amore',
+        'asia_china_imbiss',
+        'asia_thai',
+        'athos',
+        '_hakis',
+        'beirut',
+        'celery',
+        'chicos',
+        'fine',
+        'fortuna',
+        'gold_dragon',
+        'hakis',
+        'holy rice',
+        'indisch',
+        'just_lecker',
+        'khaohom_thai',
+        'orchid',
+        'pak-royal',
+        'pizza_cab',
+        'round_two',
+        'royal_3',
+        'sinbads',
+        'tandoori_palace',
+        'zu_dis_asia',
+    ];
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
@@ -48,10 +129,10 @@ final class RestaurantFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            'background_url' => self::faker()->randomElement(self::RANDOM_PHOT_PATH),
+            'background_url' => self::faker()->randomElement(self::BACKGROUND_PHOTOS),
             'logoFile' => self::faker()->imageUrl(),
-            'logoUrl' => self::faker()->randomElement(self::RANDOM_PHOT_PATH),
-            'name' => self::faker()->text(),
+            'logoUrl' => self::faker()->randomElement(self::LOGOS),
+            'name' => self::faker()->randomElement(self::NAMES),
             'shopUrl' => self::faker()->url(),
             'categories' => [self::faker()->name(), self::faker()->name()],
         ];
@@ -62,9 +143,8 @@ final class RestaurantFactory extends ModelFactory
      */
     protected function initialize(): self
     {
-        return $this
-            // ->afterInstantiate(function(Restaurant $restaurant): void {})
-        ;
+        return $this// ->afterInstantiate(function(Restaurant $restaurant): void {})
+            ;
     }
 
     protected static function getClass(): string
